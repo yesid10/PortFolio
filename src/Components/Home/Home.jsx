c20b0012ed94785bff8e0797301e2212e6df7106
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-04-09 23:39:26 -0500

    Estilando botones, arreglando el boton de video y ajustando el home para que este centrado

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,12 @@
-import { HiOutlineArrowUpRight } from "react-icons/hi2";
+import { ImArrowUpRight2 } from "react-icons/im";
 import { IoLogoLinkedin } from "react-icons/io5";
 import { SiGithub } from "react-icons/si";
 import { FaInstagram } from "react-icons/fa";
 import "./StylesHome.scss"
-import fotoHojaVida from "../../assets/Diseño sin título.png";
-import logoVideo from '../../assets/Logo_video.png';
+import fotoHojaVida from "../../assets/Foto_Hoja_de_Vida-sin_fondo.png";
+import logoVideo from '../../assets/Logotipo tejido en lana tipográfico marrón.jpg';
 import { FaPlay } from "react-icons/fa6";
+import { TbBrandTelegram } from "react-icons/tb";
 
 
 
@@ -15,7 +16,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home__info">
-        <div>
+        <div className="name">
           <h1>Yesid Vanegas ✌️</h1>
         </div>
         <div className="home__info__rol">
@@ -27,11 +28,11 @@ const Home = () => {
             I`m creative designer based in New York, and I`m very passionate and dedicated to my work.
           </p>
         </div>
-        <div>
-          <button>Say Hello</button>
-          <button>My works <HiOutlineArrowUpRight /> </button>
+        <div className="buttonsWorks">
+          <button className="hello">Say Hello <TbBrandTelegram style={{backgroundColor:'transparent', marginLeft:'10px', fontSize:'1.5rem'}}/></button>
+          <button className="works">My works < ImArrowUpRight2  style={{fontSize:'1.5rem', marginLeft:'10px'}} /> </button>
         </div>
-        <div>
+        <div className="buttonsFollow">
           <p>Sigueme: </p>
           <button><IoLogoLinkedin /></button>
           <button><SiGithub /></button>
@@ -43,14 +44,15 @@ const Home = () => {
       </div>
 
       <div className="home__imagen">
-        <figure>
+        <div className="fotoPerfil">
           <img className="fotoPerfil" src={fotoHojaVida} alt="yo" />
-        </figure>
+        </div>
+        <div className="logoVideo">
+          <img src={logoVideo} alt="video" />
+          <button className="buttonVideo"><FaPlay /></button>
+        </div>
 
-        <img className="logoVideo" src={logoVideo} alt="video" />
-        <button className="buttonVideo"><FaPlay /></button>
-        
-       
+
       </div>
     </div>
   )
