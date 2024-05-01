8a73986e638ec43ae2d686a367a82b64d7d64283
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-05-01 00:28:05 -0500

    Pagina de about estilada y terminada para dar funcionalidad

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,15 +1,39 @@
 import "./StylesAbout.scss";
 
+import { IoDocumentTextOutline } from "react-icons/io5";
+
 const About = () => {
   return (
     <div className="about">
       <div className="about__experiencia">
-        <h3>10</h3>
+        <h3 className="years_experienci">01</h3>
         <span>Año de experiencia</span>
-        <h3>Yesid Vanegas</h3>
+        <h3 className="name">Yesid Vanegas</h3>
       </div>
       <div className="about__acercademi">
-        <span>svg</span>
+        <span className="figure"><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="86"
+          height="84"
+          fill="none"
+          className="svg replaced-svg"
+          viewBox="0 0 86 84"
+        >
+          <path
+            stroke="#FED546"
+            strokeWidth="3"
+            d="M69.058 4.995c-3.735.624-13.204 5.856-12.64 14.506m0 0c.161 2.467.391 5.668 3.215 8.008 2.954 2.447 8.865-3.4-3.215-8.008zm0 0c-6.836-2.405-21.423-3.53-25.08 11.21-2.246 11.614 3.5 25.61 6.583 34.838-7.104-11.077-25-24.026-34.585-25.227"
+          ></path>
+          <path
+            fill="#FED546"
+            d="M67.81 9.302l1.058-4.54-4.552-1.006.327-1.4 5.956 1.316-1.385 5.94-1.404-.31z"
+          ></path>
+          <path
+            stroke="#FED546"
+            strokeWidth="3"
+            d="M26.009 78.456c.08-1.274.68-4.651 2.436-7.972M4.841 78.326c3.405-3.658 12.031-11.456 19.298-13.39M55.398 5.34c-1.723-.097-5.107 3.521-4.075 7.117"
+          ></path>
+        </svg></span>
         <h3>Acerca de mi <span></span></h3>
         <p>With 10 years experience as a professional Web developer,
           I have acquired the skills and knowledge necessary to make
@@ -19,6 +43,7 @@ const About = () => {
         <div className="firma">
           <button>
             Descargar CV
+            <IoDocumentTextOutline style={{ fontSize: '1.5rem', fontWeight:'700', marginLeft:'.4rem' }} />
           </button>
           <img src="https://orido-react.vercel.app/img/signature.png" alt="firma" />
         </div>
