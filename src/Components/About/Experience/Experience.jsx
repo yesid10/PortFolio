345b67d83a616eb2f7dde64dbd1640f248fec8f6
Yesid Vanegas <yesidvanegas68@gmail.com>
2024-06-12 21:43:08 -0500

    creando array para consumir localmente en las skills

--- src/Components/About/Experience/Experience.jsx
+++ src/Components/About/Experience/Experience.jsx
@@ -1,11 +1,29 @@
-
+import { TbBrandTelegram } from "react-icons/tb";
 import './StylesExperience.scss';
 
 
 const Experience = () => {
+
+
+  const arraySkills = [
+    {
+      id: 1,
+      icon: <TbBrandTelegram/>,
+    }
+  ];
+
+
   return (
     <div className="experienciaFuction">
-     Experiencias
+     {
+      arraySkills?.map(skill => (
+        <div key={skill.id}>
+          yesid
+          {skill.id}
+          {skill.icon}
+        </div>
+      ))
+     }
     </div>
   )
 }
